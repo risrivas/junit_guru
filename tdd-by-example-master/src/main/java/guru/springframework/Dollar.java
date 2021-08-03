2e7285b2b021ba65823cac2487576b88795289b8
Rishi Srivastava <rishisrivasta@gmail.com>
2021-08-03 08:17:48 +0800

    completed tdd2

--- tdd-by-example-master/src/main/java/guru/springframework/Dollar.java
+++ tdd-by-example-master/src/main/java/guru/springframework/Dollar.java
@@ -4,13 +4,17 @@ package guru.springframework;
  * Created by rishi on 28/07/2021
  */
 public class Dollar {
-    int amount;
+    private int amount;
 
     public Dollar(int amount) {
         this.amount = amount;
     }
 
-    public void times(int multiplier) {
-        amount *= multiplier;
+    public Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+
+    public int getAmount() {
+        return amount;
     }
 }
--- tdd-by-example-master/src/test/java/guru/springframework/MoneyTest.java
+++ tdd-by-example-master/src/test/java/guru/springframework/MoneyTest.java
@@ -14,7 +14,10 @@ public class MoneyTest {
     @DisplayName("$5 * 2 = $10")
     void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        Dollar product = five.times(2);
+        assertEquals(10, product.getAmount());
+
+        product = five.times(3);
+        assertEquals(15, product.getAmount());
     }
 }
