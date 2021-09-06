b213ef0a79040a7226f21aee9c9b6014fcd1d5aa
Rishi Srivastava <rishisrivasta@gmail.com>
2021-09-07 06:38:26 +0800

    conditional testing

--- testing-java-junit5-master/src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ testing-java-junit5-master/src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
 
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 /**
  * Created by rishi on 02/09/2021
@@ -57,4 +59,52 @@ class IndexControllerTest {
             System.out.println("I got here 123414341");
         });
     }
+
+    @Test
+    void testAssumptionTrue() {
+        assumeTrue("GURU".equalsIgnoreCase(System.getenv("GURU_RUNTIME")));
+    }
+
+    @Test
+    void testAssumptionTrueAssumptionIsTrue() {
+        assumeTrue("GURU".equalsIgnoreCase("GURU"));
+    }
+
+    @EnabledOnOs(OS.MAC)
+    @Test
+    void testMeOnMacOS() {
+
+    }
+
+    @EnabledOnOs(OS.WINDOWS)
+    @Test
+    void testMeOnWindows() {
+
+    }
+
+    @EnabledOnJre(JRE.JAVA_8)
+    @Test
+    void testMeOnJava8() {
+
+    }
+
+    @EnabledOnJre(JRE.JAVA_11)
+    @Test
+    void testMeOnJava11() {
+
+    }
+
+    @EnabledIfEnvironmentVariable(named = "username", matches = "rishi")
+    @Test
+    void testIfUserRishi() {
+
+    }
+
+    @EnabledIfEnvironmentVariable(named = "username", matches = "fred")
+    @Test
+    void testIfUserFred() {
+
+    }
+
+
 }
