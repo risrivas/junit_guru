d27bd389acd0a47c4c5f089d6e477653c5ec1044
Rishi Srivastava <rishisrivasta@gmail.com>
2021-09-11 07:49:53 +0800

    completed assignment

--- testing-java-junit5-master/src/test/java/guru/springframework/sfgpetclinic/controllers/VetControllerTest.java
+++ testing-java-junit5-master/src/test/java/guru/springframework/sfgpetclinic/controllers/VetControllerTest.java
@@ -2,6 +2,7 @@ package guru.springframework.sfgpetclinic.controllers;
 
 import guru.springframework.sfgpetclinic.fauxspring.Model;
 import guru.springframework.sfgpetclinic.fauxspring.ModelMapImpl;
+import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.SpecialtyService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import guru.springframework.sfgpetclinic.services.map.SpecialityMapService;
@@ -9,7 +10,9 @@ import guru.springframework.sfgpetclinic.services.map.VetMapService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Created by rishi on 10/09/2021
@@ -27,11 +30,20 @@ class VetControllerTest {
         vetService = new VetMapService(specialtyService);
         vetController = new VetController(vetService);
         model = new ModelMapImpl();
+
+        Vet vet1 = new Vet(1L, "Joe", "Buck", null);
+        Vet vet2 = new Vet(2L, "Jimmy", "Smith", null);
+
+        vetService.save(vet1);
+        vetService.save(vet2);
     }
 
     @Test
     void listVets() {
         String index = vetController.listVets(model);
         assertEquals("vets/index", index);
+
+        final Set vets = (Set) ((ModelMapImpl) model).getMap().get("vets");
+        assertEquals(2, vets.size());
     }
 }
--- testing-java-junit5-master/src/test/java/guru/springframework/sfgpetclinic/fauxspring/ModelMapImpl.java
+++ testing-java-junit5-master/src/test/java/guru/springframework/sfgpetclinic/fauxspring/ModelMapImpl.java
@@ -1,16 +1,26 @@
 package guru.springframework.sfgpetclinic.fauxspring;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by rishi on 10/09/2021
  */
-public class ModelMapImpl implements Model{
+public class ModelMapImpl implements Model {
+
+    public Map<String, Object> getMap() {
+        return map;
+    }
+
+    private final Map<String, Object> map = new HashMap<>();
+
     @Override
     public void addAttribute(String key, Object o) {
-
+        map.put(key, o);
     }
 
     @Override
     public void addAttribute(Object o) {
-
+        // do nothing
     }
 }
