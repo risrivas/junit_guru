b0d44490be8cefa37f695c622bf5e3246c5185ad
Rishi Srivastava <rishisrivasta@gmail.com>
2021-10-02 08:48:46 +0800

    mockito verify

--- testing-junit5-mockito-master/src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ testing-junit5-mockito-master/src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -8,6 +8,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.*;
+
 /**
  * Created by rishi on 01/10/2021
  */
@@ -21,8 +26,47 @@ class SpecialitySDJpaServiceTest {
     SpecialitySDJpaService service;
 
     @Test
+    void findByIdTest() {
+        Speciality speciality = new Speciality();
+        when(specialtyRepository.findById(1L)).thenReturn(Optional.of(speciality));
+
+        Speciality foundSpeciality = service.findById(1L);
+
+        assertThat(foundSpeciality).isNotNull();
+        verify(specialtyRepository).findById(1L);
+    }
+
+    @Test
     void deleteById() {
         service.deleteById(1L);
+        service.deleteById(1L);
+
+        verify(specialtyRepository, times(2)).deleteById(1L);
+    }
+
+    @Test
+    void deleteByIdAtLeast() {
+        service.deleteById(1L);
+        service.deleteById(1L);
+
+        verify(specialtyRepository, atLeastOnce()).deleteById(1L);
+    }
+
+    @Test
+    void deleteByIdAtMost() {
+        service.deleteById(1L);
+        service.deleteById(1L);
+
+        verify(specialtyRepository, atMost(5)).deleteById(1L);
+    }
+
+    @Test
+    void deleteByIdNever() {
+        service.deleteById(1L);
+        service.deleteById(1L);
+
+        verify(specialtyRepository, atLeastOnce()).deleteById(1L);
+        verify(specialtyRepository, never()).deleteById(4L);
     }
 
     @Test
---
+++ testing-junit5-mockito-master/src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/VetSDJpaServiceTest.java
@@ -0,0 +1,31 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.repositories.VetRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by rishi on 02/10/2021
+ */
+@ExtendWith(MockitoExtension.class)
+class VetSDJpaServiceTest {
+
+    @Mock
+    private VetRepository vetRepository;
+
+    @InjectMocks
+    private VetSDJpaService service;
+
+    @Test
+    void deleteById() {
+        service.deleteById(1L);
+
+        verify(vetRepository, times(1)).deleteById(1L);
+    }
+}
